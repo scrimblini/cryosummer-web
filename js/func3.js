eb95553b1f7a477da6c3b0f79d2923a621f078bf
scrimblini <102263436+scrimblini@users.noreply.github.com>
2025-03-09 05:21:12 -0300

    Fixed responsive design in a few pages

---
+++ js/func3.js
@@ -0,0 +1,18 @@
+//news
+        function showPopup(popupId) {
+            document.getElementById(popupId).style.display = "flex";
+        }
+
+        function closePopup(popupId) {
+            document.getElementById(popupId).style.display = "none";
+        }
+
+//shop
+
+function showItem() {
+    document.getElementById("popup").style.display = "flex";
+}
+
+function closeItem() {
+    document.getElementById("popup").style.display = "none";
+}
